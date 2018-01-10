5ecff35a18d2b22fbbbe0507ec3701697b0f8f12
kendy92 <dinhconganh@gmail.com>
2018-01-10 18:29:37 -0500

    Add FAQ and Recruitment Features

---
+++ faq/faq_page/faq_cms.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" ValidateRequest="false" CodeBehind="faq_cms.aspx.cs" Inherits="faq_page.faq_cms" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>FAQ Admin Page</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800" rel="stylesheet"/>
+    <link href="style.css" rel="stylesheet"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>FAQ Admin Page</h1>
+        <div class="form-control">
+            <asp:Label runat="server">ID</asp:Label>
+            <asp:TextBox ID="txt_id" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-control">
+            <asp:Label runat="server">Question</asp:Label>
+            <asp:TextBox ID="txt_question" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-control">
+            <asp:Label runat="server">Answer</asp:Label>
+            <asp:TextBox CssClass="text-area" ID="text_answer" TextMode="MultiLine" runat="server"></asp:TextBox>
+        </div>
+        <div id="err_msg" runat="server"></div>
+        <div class="form-control">
+            <asp:Button CssClass="btn" ID="btnShow" Text="Show All" runat="server" OnClick ="btnShow_Click" />
+            <asp:Button CssClass="btn" ID="btnFind" Text="Find By Id" runat="server" OnClick="btnFind_Click" />
+            <asp:Button CssClass="btn" ID="btnInsert" Text="Add Question" runat="server" OnClick="btnInsert_Click" />
+            <asp:Button CssClass="btn" ID="btnUpdate" Text="Edit Question" runat="server" OnClick="btnUpdate_Click" />
+            <asp:Button CssClass="btn" ID="btnDelete" Text="Delete Question" runat="server" OnClick="btnDelete_Click" />
+            <asp:Button CssClass="btn" ID="btnFAQPage" Text="FAQ Page" runat="server" OnClick="btnFAQPage_Click" />
+        </div>
+        <div id="result" runat="server"></div>
+    </form>
+</body>
+</html>
---
+++ recruitment/recruitment/recruitment_cms.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using recruitment.Models;
+
+namespace recruitment
+{
+    public partial class recruitment_cms : System.Web.UI.Page
+    {
+        recruitment_feature r = new recruitment_feature();
+        DateTime today = DateTime.Now;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            result.InnerHtml = r.showRecruitmentData();
+            result.Style.Add("display", "block");
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            if(txt_id.Text.Length <= 0 || txt_id.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job ID";
+                txt_id.Focus();
+            }
+            else if (txt_title.Text.Length <= 0 || txt_title.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job Title";
+                txt_title.Focus();
+            }
+            else if (txt_desc.Text.Length <= 0 || txt_desc.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job Description";
+                txt_desc.Focus();
+            }
+            else if (txt_requirement.Text.Length <= 0 || txt_requirement.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job Requirement";
+                txt_requirement.Focus();
+            }
+            else if (job_category_list.Text.Length <= 0 || job_category_list.Text == "")
+            {
+                err_msg.InnerHtml = "Please select Job Category";
+                job_category_list.Focus();
+            }
+            else if (job_division_list.Text.Length <= 0 || job_division_list.Text == "")
+            {
+                err_msg.InnerHtml = "Please select Job Division";
+                job_division_list.Focus();
+            }else
+            {
+                r.job_id = Convert.ToInt32(txt_id.Text);
+                r.job_title = txt_title.Text;
+                r.job_desc = Server.HtmlEncode(txt_desc.Text);
+                r.job_require = Server.HtmlEncode(txt_requirement.Text);
+                r.job_category = job_category_list.Text;
+                r.job_division = job_division_list.Text;
+                r.job_postdate = Convert.ToDateTime(today.ToString("yyyy-MM-dd"));
+                r.insertRow();
+                err_msg.InnerHtml = r.err_Msg;
+            }
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txt_id.Text.Length <= 0 || txt_id.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job ID";
+                txt_id.Focus();
+            }
+            else if (txt_title.Text.Length <= 0 || txt_title.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job Title";
+                txt_title.Focus();
+            }
+            else if (txt_desc.Text.Length <= 0 || txt_desc.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job Description";
+                txt_desc.Focus();
+            }
+            else if (txt_requirement.Text.Length <= 0 || txt_requirement.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job Requirement";
+                txt_requirement.Focus();
+            }
+            else if (job_category_list.Text.Length <= 0 || job_category_list.Text == "")
+            {
+                err_msg.InnerHtml = "Please select Job Category";
+                job_category_list.Focus();
+            }
+            else if (job_division_list.Text.Length <= 0 || job_division_list.Text == "")
+            {
+                err_msg.InnerHtml = "Please select Job Division";
+                job_division_list.Focus();
+            }
+            else
+            {
+                r.job_id = Convert.ToInt32(txt_id.Text);
+                r.job_title = txt_title.Text;
+                r.job_desc = Server.HtmlEncode(txt_desc.Text);
+                r.job_require = Server.HtmlEncode(txt_requirement.Text);
+                r.job_category = job_category_list.Text;
+                r.job_division = job_division_list.Text;
+                r.job_postdate = Convert.ToDateTime(today.ToString("yyyy-MM-dd"));
+                r.updateRow();
+                err_msg.InnerHtml = r.err_Msg;
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txt_id.Text.Length <= 0 || txt_id.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job ID!";
+                txt_id.Focus();
+            }
+            else
+            {
+                r.job_id = Convert.ToInt32(txt_id.Text);
+                r.deleteRow();
+                err_msg.InnerHtml = r.err_Msg;
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txt_id.Text.Length <= 0 || txt_id.Text == "")
+            {
+                err_msg.InnerHtml = "Please enter Job ID";
+                txt_id.Focus();
+            }
+            else
+            {
+                r.job_id = Convert.ToInt32(txt_id.Text);
+                r.findJobById(txt_title, txt_desc, txt_requirement, job_division_list, job_category_list);
+                err_msg.InnerHtml = r.err_Msg;
+            }
+        }
+
+        protected void btnRecruitPage_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("recruitment_page.aspx");
+        }
+    }
+}
